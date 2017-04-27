fe0273d6b024eb3ccb7e2ae2db4fe3f517b47545
Mostafa <mostafa.rafaie@gmail.com>
2017-04-27 01:30:07 -0500

    Add some updates!

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-﻿from flask import Flask, render_template, request
+﻿from flask import Flask, render_template, request, make_response
+from functools import wraps, update_wrapper
+from datetime import datetime
 
 import logging
 
 logger = logging.getLogger(__name__)
 
+
+def nocache(view):
+	@wraps(view)
+	def no_cache(*args, **kwargs):
+		response = make_response(view(*args, **kwargs))
+		response.headers['Last-Modified'] = datetime.now()
+		response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+		response.headers['Pragma'] = 'no-cache'
+		response.headers['Expires'] = '-1'
+		print('asasas')
+		return response
+
+	return update_wrapper(no_cache, view)
 
 #######################
 #### configuration ####
@@ -17,6 +32,7 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 #### routes ####
 ################
 @app.route('/index', methods=['GET'])
+@nocache
 def index():
 	return render_template('index.html')
 
