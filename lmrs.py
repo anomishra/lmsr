2a482b1383dae6d0bffe8a26b7091810572e5980
Mostafa <mostafa.rafaie@gmail.com>
2017-04-28 09:46:51 -0500

    Store History features

--- lmrs.py
+++ lmrs.py
@@ -10,13 +10,14 @@ class LMRS:
     investors = {}
     orders = []
     day = 1
-    MAX_BUY = 60
+    MAX_BUY = 20
     MAX_ORDER_SHOW = 30
+    history_data = {}
 
     @staticmethod
     def init(investor_count, b_number, budget=500):
         print("Init ...... .")
-        day = 1
+        LMRS.day = 1
         LMRS.investor_count = investor_count
         LMRS.b_number = b_number
         LMRS.orders = []
@@ -26,6 +27,14 @@ class LMRS:
             investor = Investor(id = i, budget = budget)
             LMRS.investors[i] = investor
         LMRS.update_market(0, 0, 0, 0, 0)
+        LMRS.history_data = {'1':{'DH_DS16': 0, 'DH_RS16': 0, 'RH_DS16': 0, 'RH_RS16':0, 'OTHER16':0}}
+
+        Market.DH_DS16 = 0
+        Market.DH_RS16 = 0
+        Market.RH_DS16 = 0
+        Market.RH_RS16 = 0
+        Market.OTHER16 = 0
+
         return jsonify({'state':True})
 
     @staticmethod
@@ -73,6 +82,14 @@ class LMRS:
         Market.RH_RS16_u_price = LMRS.calc_c(0, 0, 0, 1, 0)
         Market.OTHER16_u_price = LMRS.calc_c(0, 0, 0, 0, 1)
 
+        LMRS.history_data[LMRS.day] = {'DH_DS16': Market.DH_DS16,
+                                       'DH_RS16': Market.DH_RS16,
+                                       'RH_DS16': Market.RH_DS16,
+                                       'RH_RS16': Market.RH_RS16,
+                                       'OTHER16': Market.OTHER16}
+
+
+
 
     @staticmethod
     def process_order(order):
@@ -106,6 +123,26 @@ class LMRS:
             LMRS.orders.append(order)
 
     @staticmethod
+    def attach_history():
+        info = {}
+        info['DH_DS16'] = []
+        info['DH_RS16'] = []
+        info['RH_DS16'] = []
+        info['RH_RS16'] = []
+        info['OTHER16'] = []
+        info['labels'] = []
+
+        for key in LMRS.history_data:
+            info['labels'].append(int(key))
+            info['DH_DS16'].append(LMRS.history_data[key]['DH_DS16'])
+            info['DH_RS16'].append(LMRS.history_data[key]['DH_RS16'])
+            info['RH_DS16'].append(LMRS.history_data[key]['RH_DS16'])
+            info['RH_RS16'].append(LMRS.history_data[key]['RH_RS16'])
+            info['OTHER16'].append(LMRS.history_data[key]['OTHER16'])
+
+        return info
+
+    @staticmethod
     def gen_market_info():
         info = {}
         if len(LMRS.investors) == 0:
@@ -121,6 +158,7 @@ class LMRS:
             order = LMRS.orders[i]
             info['orders'][i +1] = order.get_json()
 
+        info['history'] = LMRS.attach_history()
         return info
 
     @staticmethod
